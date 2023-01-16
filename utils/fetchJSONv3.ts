e2655a612fab35615b646359a6e50e0888c2531d
Patrick Keenan <patrick.keenan.fyi@gmail.com>
2023-01-15 17:59:01 -0800

    refactor: expand typedef for fetchJSONv3 helper

--- utils/fetchJSONv3.ts
+++ utils/fetchJSONv3.ts
@@ -5,12 +5,14 @@ import { handleNon200Response } from "./handleNon200Response.ts";
 export async function fetchJSONv3(
   path: `/${string}`,
   apiKey: string,
-  params?: Record<string, string>,
+  params?: Record<string, string | number | boolean>,
 ) {
   const url = `${BASE_URL}${path}?api_key=${apiKey}`;
 
   const response = await fetch(
-    params ? `${url}&${new URLSearchParams(params)}` : url,
+    params
+      ? `${url}&${new URLSearchParams(params as Record<string, string>)}`
+      : url,
   );
 
   await handleNon200Response(response);
